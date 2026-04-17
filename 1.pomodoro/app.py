805f9014932d7e9dd435526273d3d44ba9c641e3
Kobayashi Takaki <o.share.thief@gmail.com>
2026-04-17 06:48:06 +0000

    plan ステップ1

--- 1.pomodoro/app.py
+++ 1.pomodoro/app.py
@@ -1,1 +1,12 @@
 # Pomodoro Timer App
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+	return 'Hello, World!'
+
+if __name__ == '__main__':
+	app.run(debug=True)
